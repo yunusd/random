3e5763d2e1d7bcc190fd007bff67c4d6e180cc1c
Yunus Demirpolat <yunusdemirpolatt@gmail.com>
2020-05-28 21:21:19 +0300

    add video component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Video from './Video';
+import { Container } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Container fluid>
+      <Video />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Video/Video.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState, useCallback } from 'react';
+import Youtube from 'react-youtube';
+import './video.css';
+import { Button, Alert } from 'react-bootstrap';
+
+const axios = require('axios').default;
+const apiUrl = 'https://randomvideos.herokuapp.com/api/youtube';
+
+const FORBIDDEN = 'forbidden';
+
+const Video = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [video, setVideo] = useState();
+  const [videos, setVideos] = useState();
+
+  const getMostPopularVideos = useCallback(async () => {
+    const { data } = await axios.get(apiUrl);
+    return data;
+  });
+
+  const getRandomVideoIndex = useCallback(async (videosLength) => {
+    let isNumber = false;
+    while (!isNumber) {
+      const random = Math.floor(Math.random() * videosLength);
+      let forbiddensLocalStorage = JSON.parse(localStorage.getItem(FORBIDDEN));
+      if (!forbiddensLocalStorage) {
+        localStorage.setItem(FORBIDDEN, JSON.stringify([]));
+        forbiddensLocalStorage = [];
+      }
+
+      const isForbidden = forbiddensLocalStorage.filter(
+        (forbidden) => forbidden === random
+      );
+
+      if (isForbidden.length <= 0) {
+        localStorage.setItem(
+          FORBIDDEN,
+          JSON.stringify([...forbiddensLocalStorage, random])
+        );
+        isNumber = true;
+        return random;
+      } else if (forbiddensLocalStorage.length === videosLength) {
+        localStorage.setItem(FORBIDDEN, JSON.stringify([]));
+        isNumber = true;
+      } else {
+        isNumber = false;
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const getf = async () => {
+      const data = await getMostPopularVideos();
+      const index = await getRandomVideoIndex(data.items.length);
+      setVideos(data);
+      setVideo(data.items[index]);
+      setIsLoading(false);
+    };
+    getf();
+  }, []);
+
+  const handleClick = async () => {
+    const index = await getRandomVideoIndex(videos.items.length);
+    setVideo(videos.items[index]);
+  };
+
+  if (isLoading) return <p>Yükleniyor....</p>;
+  return (
+    <>
+      <Button onClick={handleClick} className="bt" disabled={!video}>
+        {video
+          ? 'Kendimi şanslı hissediyorum :)'
+          : 'Bugünlük tüm şansınızı tükettiniz :('}
+      </Button>
+      <div className="videoWrapper">
+        {/* {videoIndex} */}
+        {video && (
+          <Youtube videoId={video.id} opts={{ height: '562', width: '349' }} />
+        )}
+        {!video && <div className="last"></div>}
+      </div>
+    </>
+  );
+};
+
+export default Video;
